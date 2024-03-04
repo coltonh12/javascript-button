5dd052af39c9479e63062d1f66fe4dab5f98cd72
Colton Hye <hyercolt@msu.edu>
2024-03-04 10:06:44 -0500

    t

--- utilities.js
+++ utilities.js
@@ -3,7 +3,7 @@ function clickotron() {
     alert('pls work');
   }
 
-  const click = document.getElementById('clickotron');
+  const click = document.getElementById('click');
 
   click.addEventListener("click", (event) => {
     clickotron();
